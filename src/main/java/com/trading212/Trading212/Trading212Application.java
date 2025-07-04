4872a03d6b62f68302583431048d5784ea7f75c1
GGSWiftPro <radoslav_petroff@abv.bg>
2025-07-04 23:22:22 +0300

    Initial commit.

---
+++ src/main/java/com/trading212/Trading212/Trading212Application.java
@@ -0,0 +1,12 @@
+package com.trading212.Trading212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trading212Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trading212Application.class, args);
+    }
+}
